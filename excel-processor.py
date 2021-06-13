7db25cf74dd17bed6ef18cc16d2435a7bfe46e95
Sean Montague <stmontague1@gmail.com>
2021-06-13 16:05:46 -0700

    Initial commit to create project respository

---
+++ excel-processor.py
@@ -0,0 +1,13 @@
+
+
+# Script to calculate ROI from the transaction history report from Coinbase
+def read_excel():
+    pass
+
+
+def main():
+    print("Starting the processing of the provided excel file.")
+    read_excel()
+
+
+main()
